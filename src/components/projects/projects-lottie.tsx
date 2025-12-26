82c01f097b58c573e513f2ec0f020512678d2dbf
abdssamie <abdessamie.elmoubarki@gmail.com>
2025-12-26 18:31:55 +0100

    fix: increased the size of the project page hero naimation

--- src/components/projects/projects-lottie.tsx
+++ src/components/projects/projects-lottie.tsx
@@ -4,7 +4,7 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export function ProjectsLottieAnimation() {
     return (
-        <div className="flex justify-center items-center w-full max-w-[350px]">
+        <div className="flex justify-center items-center w-full max-w-6xl">
             <DotLottieReact
                 src="/Animated Dashboards.lottie"
                 loop
